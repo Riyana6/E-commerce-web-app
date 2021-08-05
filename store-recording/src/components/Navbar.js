db9a959f2933002d2eaa3355b48374789844b609
JMJ Riyana <jeemariyana@gmail.com>
2021-08-06 00:13:54 +0530

    navbar-design

--- store-recording/src/components/Navbar.js
+++ store-recording/src/components/Navbar.js
@@ -1,14 +1,18 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
+import styled from 'styled-components'
 import logo from '../logo.svg'
+import { ButtonContainer } from './Button'
 export default class Navbar extends Component {
     render() {
         return (
-            <nav className="navbar bg-primary navbar-expand-sm navbar-dark px-sm05">
-                {/*https://www.iconfinder.com/icons/1243689/call_phone_icon Creative Commons (Attribution 3.0 Unported);
-                   https://www.iconfinder.com/Makoto_msk */}
+            <Nav className="navbar navbar-expand-sm navbar-dark px-sm05">
+                {/* 
+                    https://www.iconfinder.com/icons/1243689/call_phone_icon
+                    Creative Commons (Attribution 3.0 Unported);
+                    https://www.iconfinder.com/Makoto_msk */}
                 <Link to="/">
-                   <img scr={logo} alt="store" className="navbar-brand"/>
+                   <img src={logo} alt="store" className="navbar-brand"/>
                 </Link>
                 <ul className="navbar-nav align-items-center">
                    <li className="nav-item ml-5">
@@ -18,12 +22,24 @@ export default class Navbar extends Component {
                    </li>
                 </ul>
                 <Link to="/cart" className="ml-auto">
-                    <button>
+                    <ButtonContainer>
+                        <span className="mr-2">
                         <i className="fas fa-cart-plus"/>
+                        </span>
                         my cart
-                    </button>
+                    </ButtonContainer>
                 </Link>
-            </nav>
+            </Nav>
         )
     }
 }
+
+const Nav = styled.nav`
+  background: var(--mainBlue);
+  .nav-link {
+    color: var(--mainWhite) !important;
+    font-size:1.3rem;
+    text-transform:capitalize;
+    
+    }
+`

3a884fc6855039b68c26781a74c86328344a48a2
JMJ Riyana <jeemariyana@gmail.com>
2021-08-06 13:46:33 +0530

    show products

--- store-recording/src/App.js
+++ store-recording/src/App.js
@@ -8,6 +8,7 @@ import Details from './components/Details';
 import Cart from './components/Cart';
 import Default from './components/Default';
 
+
 function App() {
   return (
      <React.Fragment>
--- store-recording/src/components/Navbar.js
+++ store-recording/src/components/Navbar.js
@@ -6,28 +6,28 @@ import { ButtonContainer } from './Button'
 export default class Navbar extends Component {
     render() {
         return (
-            <Nav className="navbar navbar-expand-sm navbar-dark px-sm05">
-                {/* 
-                    https://www.iconfinder.com/icons/1243689/call_phone_icon
-                    Creative Commons (Attribution 3.0 Unported);
-                    https://www.iconfinder.com/Makoto_msk */}
+            <Nav className="navbar navbar-expand-sm  navbar-dark px-sm-5">
+            {/* 
+    https://www.iconfinder.com/icons/1243689/call_phone_icon
+    Creative Commons (Attribution 3.0 Unported);
+    https://www.iconfinder.com/Makoto_msk */}
                 <Link to="/">
-                   <img src={logo} alt="store" className="navbar-brand"/>
+                <img src={logo} alt="store" className="navbar-brand" />
                 </Link>
                 <ul className="navbar-nav align-items-center">
-                   <li className="nav-item ml-5">
-                       <Link to="/" className="nav-link">
-                           products
-                       </Link>
-                   </li>
+                <li className="nav-item ml-5">
+                    <Link to="/" className="nav-link">
+                    products
+                    </Link>
+                </li>
                 </ul>
                 <Link to="/cart" className="ml-auto">
-                    <ButtonContainer>
-                        <span className="mr-2">
-                        <i className="fas fa-cart-plus"/>
-                        </span>
-                        my cart
-                    </ButtonContainer>
+                <ButtonContainer>
+                    <span className="mr-2">
+                    <i className="fas fa-cart-plus " />
+                    </span>
+                    my cart
+                </ButtonContainer>
                 </Link>
             </Nav>
         )
@@ -40,6 +40,8 @@ const Nav = styled.nav`
     color: var(--mainWhite) !important;
     font-size:1.3rem;
     text-transform:capitalize;
-    
-    }
-`
+  }
+  @media (max-width: 576px) {
+    .navbar-nav {
+      flex-direction: row !important;
+`;
